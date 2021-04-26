fdc25322755125c0d4b325110d4a40f2d01e46e6
JordanVegar <jve31@uclive.ac.nz>
2021-04-26 18:14:41 +1200

    Description for main. Mostly checking commit is working.

--- src/main/main.java
+++ src/main/main.java
@@ -1,5 +1,12 @@
 package main;
 
+/**
+ * The entry point to the program, creates instances of the classes required 
+ * to start the game, then handles control to the class for the desired ui.
+ * 
+ * @author Jordan Vegar and Hugo Phibbs
+ *
+ */
 public class main {
 	
 	public static void main(String[] args) {
