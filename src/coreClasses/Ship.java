b5504d6d3a07a8a38355aa747a8699a1640ce6fe
Jordan Vegar <jve31@uclive.ac.nz>
2021-05-02 13:15:59 +1200

    Added a setSail method to ship. Needs you to implement the playing out of random events.

--- src/coreClasses/Ship.java
+++ src/coreClasses/Ship.java
@@ -352,7 +352,19 @@ public class Ship {
      * 
      * @param owner Player object for owner of the ship
      */
-    public void setOwner(Player owner) {this.owner = owner;}   
+    public void setOwner(Player owner) {this.owner = owner;}  
+    
+    public void setSail(Route route, GameEnvironment ge) {
+    	// Repair ship and pay wages before setting sail.
+		repairShip();
+		payWages(route, owner);
+		// Set sail
+		// TODO run random events. 
+		// Arrive at new island
+		int routeDuration = route.getDistance() / speed;
+		ge.reduceDaysRemaining(routeDuration);
+		ge.setCurrentIsland(route.getDestination());		
+    }
     
     
    // TODO implement bellow?
