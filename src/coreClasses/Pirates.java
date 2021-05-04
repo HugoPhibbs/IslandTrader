f64fe552f54f514bb0343df291442bf1659e5d14
hugophibbs <81545721+hugophibbs@users.noreply.github.com>
2021-05-04 15:07:10 +1200

    Changes to Pirate Class
    Added new methods that handle basically all the functionality of the
    specs.
    
    should be ready to test.

--- src/coreClasses/Pirates.java
+++ src/coreClasses/Pirates.java
@@ -1,32 +1,76 @@
 package coreClasses;
 
-public class Pirates {
-    // pirates doesnt need to actually have an object, thus all its methods can be static
-    private String description;
-    private int goodDemand;
+import exceptions.GameOverException;
+import java.util.Random;
+import java.util.ArrayList;
 
+public class Pirates {
+	// in ui, it separately calls, getDescription(), rollDice(), attackShip(int diceInt, Ship ship)
+	// and in that order
+    
     public static String getDescription(){
-        // something like "Pirates, roll the die to play your chances!"
     	return "You have encountered pirates, roll the die to play your chances!";
     }
 
-    public static void attackShip(Ship ship) {
+    public static boolean attackShip(int diceInt, Ship ship) {
     	
+    	/* based on a game of chance
+    	 * defense capability is multiplied by the number that is rolled by dice. 
+    	 * random int is chosen because max defense capability is 50, tf max effectivity is
+    	 * 300, so there is a chance of 18% of loosing with a perfect turn!, max dice
+    	 * roll and max defense capability
+    	 */
+    	
+    	Random random = new Random();
+    	int randomAttack = random.nextInt(366); //arbitrary upper bound, can be adjusted if need be
+    	if (randomAttack > ship.getDefenseCapability()*diceInt) {
+    		takeGoods(ship);
+    		return false;
+    	}
+    	return true;
     }
 
-    private void takeGoods(Ship ship){
-        // takes goods from a ships cargo hol
-        // demand for goods is decided by a random number generator
-        // creates a random goods demand
-        // int goodDemand = randNumber... idk how to do this, do it later
-        if (goodDemand > ship.getOccupiedCargoCapacity()){
-            // ships crew have to walk the plank.
-            // GAMEOVER
+    private static void takeGoods(Ship ship){
+    	Random random = new Random();
+    	int randomGoodDemand = random.nextInt(120); //arbitrary upper bound, can be adjusted if need be
+    	    			
+        if (randomGoodDemand > ship.getOccupiedCargoCapacity()){
+        	/* IMPORTANT due to this implementation, item and ship upgrades must be 
+        	 * stored separately within a ship!
+        	 */
+        	String gameOverMessage = "You have less goods than what the pirates demand. \n"
+        			+ "You and your crew have to walk the plank! \n"
+        			+ "GAME OVER!";
+        	throw new GameOverException(gameOverMessage);
         }
         else{
-            // how to we implement pirates taking items.
-            // do we take items from teh itemArrayList until the demand is satisfied.
-            // in this case you take more than demanded.
+        	while (randomGoodDemand > 0) {
+        		 Item biggestItem = getLargestShipItem(ship);
+        		 ship.takeItem(biggestItem.getName());
+        		 randomGoodDemand -= biggestItem.getSpaceTaken();
+        	}
         }
+    }
+    
+    public static int rollDice() {
+    	// ask for ui to rollDie, then print out what number was rolled, should
+    	// be a button in gui or entry in cmd line ui to roll a dice
+    	
+    	// here to make experience more interactive in the ui, like actually rolling a dice
+    	Random random = new Random();
+    	return random.nextInt(6) + 1;
+    }
+    
+    private static Item getLargestShipItem(Ship ship) {
+    	ArrayList<Item> itemsArrayList = ship.getItems();
+    	int biggestSize = -1;
+    	Item biggestItem = null;
+    	for (int i = 0; i < itemsArrayList.size(); i++) {
+    		if (itemsArrayList.get(i).getSpaceTaken() > biggestSize) {
+    			biggestItem = itemsArrayList.get(i);
+    			biggestSize = biggestItem.getSpaceTaken();
+    		}
+    	}
+    	return biggestItem;
     }
 }
