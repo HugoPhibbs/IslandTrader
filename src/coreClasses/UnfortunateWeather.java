2c0723af8573a2a53258a56883ea1ddf07aa343a
hugophibbs <81545721+hugophibbs@users.noreply.github.com>
2021-05-04 15:12:21 +1200

    Changes to Unfortunate Weather
    added functionality up to what is required in specs

--- src/coreClasses/UnfortunateWeather.java
+++ src/coreClasses/UnfortunateWeather.java
@@ -1,6 +1,18 @@
 package coreClasses;
 
+import exceptions.GameOverException;
+import java.util.Random;
+
 public class UnfortunateWeather{
-	// TODO Implement!
+
+	public static String getDescription() {
+		return "You have entered a dangerous storm, you are at the mercy of the weather!";
+	}
+	
+	public static void damageShip(Ship ship) {
+		Random random = new Random();
+		int damage = random.nextInt(100); // max damage of 99, cant end game
+		ship.takeDamage(damage);
+	}
 }
 
