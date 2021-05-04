9252a01bb3582ab8c35068969e5f457fda621db8
Jordan Vegar <jve31@uclive.ac.nz>
2021-05-05 11:17:26 +1200

    Update to GameUi interface.
    Added a starting money variable, so starting money can be adjusted in
    one place, and have that change carry through to both CLUI and GUI

--- src/uiClasses/GameUi.java
+++ src/uiClasses/GameUi.java
@@ -4,20 +4,18 @@ import coreClasses.*;
 
 public interface GameUi {
 	
-	/**
-	 * String describing the name requirements, to be displayed/printed if the name entered is invalid.
-	 */
+	// String describing the name requirements, to be displayed/printed if the name entered is invalid.
 	String NAME_REQUIREMENTS = "Invalid name. Please enter a name with 3-15 letters, no numbers or special characters.";
 	
-	/**
-	 * String describing the valid range of days to be chosen to be played.
-	 */
+	// String describing the valid range of days to be chosen to be played.
 	String DURATION_REQUIREMENTS = "Invalid game duration. Please enter a number of days (integer) between 20 and 50";
 	
+	// The amount of money the player starts with.
+	int STARTING_MONEY = 100;
 	
 	public void setup(GameEnvironment gameEnvironment);
 	
 	public void playGame();
 	
-	public void finishGame();
+	public void finishGame(String message);
 }
