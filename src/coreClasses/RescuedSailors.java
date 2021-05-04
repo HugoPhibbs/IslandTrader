7812156bd8cd713c0cafeec13b869edbcedb02d3
hugophibbs <81545721+hugophibbs@users.noreply.github.com>
2021-05-04 15:08:04 +1200

    Written RescuedSailors class.
    gives a constant amount of money to a player, no mention of randomness
    in specs.

--- src/coreClasses/RescuedSailors.java
+++ src/coreClasses/RescuedSailors.java
@@ -1,7 +1,12 @@
 package coreClasses;
-//hello
 
 public class RescuedSailors {
 	
-	
+	public static String getDescription() {
+		return "You have encountered stranded sailors. \n You have recieved 50 Pirate Bucks as a reward for rescuing them!";
+	}
+
+	public static void giveMoney(Player player) {
+		player.earnMoney(50); // arbitrary value, not based on chance in specs, so just make it 50
+	}
 }
