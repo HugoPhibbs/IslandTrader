e257d4b0f063e1e651cf4bd2d9ce9dae55ffbc72
hugophibbs <81545721+hugophibbs@users.noreply.github.com>
2021-05-06 11:34:37 +1200

    Changes to GE
    Removed superfluous code, that now exists in the main method.

--- src/coreClasses/GameEnvironment.java
+++ src/coreClasses/GameEnvironment.java
@@ -90,6 +90,8 @@ public class GameEnvironment {
 		this.daysRemaining = duration;
 		this.currentIsland = startisland;
 		
+		player.setShip(ship);
+		
 		ui.playGame();
 	}
 	
@@ -154,12 +156,12 @@ public class GameEnvironment {
 		return otherIslands;
 	}
 	
-	public void buyFromStore(String itemToBuyName) {
-		currentIsland.getIslandStore().buyItem(itemToBuyName, player);
+	public Item buyFromStore(String itemToBuyName) {
+		return currentIsland.getIslandStore().sellItem(itemToBuyName, player);
 	}
 	
-	public void sellToStore(String itemToSellName) {
-		currentIsland.getIslandStore().buyItem(itemToSellName, player);
+	public Item sellToStore(String itemToSellName) {
+		return currentIsland.getIslandStore().buyItem(itemToSellName, player);
 	}
 	
 	public ArrayList<String> getShipDescriptionArrayList() {
@@ -193,7 +195,7 @@ public class GameEnvironment {
 	 */
 	public int minMoneyRequired() {
 		int cost = ship.getRepairCost();
-		cost += ship.getWageCost(shortestRoute());
+		cost += ship.getRouteWageCost(shortestRoute());
 		return cost;
 	}
 	
@@ -225,40 +227,6 @@ public class GameEnvironment {
 	 * Still keep it organised tho so its readable. 5th island coming to a game near you soon.
 	 */
 	
-	// TODO migrate this entire method into the main class
-	public static void main(String[] args) {
-		Store s1 = new Store(); Store s2 = new Store(); Store s3 = new Store(); 
-		Island i1 = new Island("other 1", s1, "arb1"); 
-		Island i2 = new Island("other 2", s2, "arb2"); 
-		Island i3 = new Island("other 3", s3, "arb3"); 
-		
-		/* TODO
-		 * when creating a new store the store needs to reference the island that it belongs to
-		 * a two way relationship like player and ship, method setIslandStore in store allows this
-		 */
-		// Creating current instance of Island
-		Store currStore = new Store(); 
-		Island currentIsland = new Island("current", currStore, "arb description");
-		// creating routes from the current island
-		Route r11 = new Route("r11", 3, currentIsland, i1, "wavy route, but with storms come sailors to save");
-		Route r12 = new Route("r12", 2, currentIsland, i1, "easy sailing, which pirates also enjoy");
-		Route r21 = new Route("r21", 2, currentIsland, i2, "safe route, low risk, low reward.");
-		Route r22 = new Route("r22", 2, currentIsland, i2, "wavy route, but with storms come sailors to save");
-		Route r31 = new Route("r31", 4, currentIsland, i3, "long route, takes a detour to find calmer waters");
-		Route r32 = new Route("r32", 1, currentIsland, i3, "Direct route with high winds, fast but lots of bad weather. ");
-		// create route array and pass to current island
-		Route[] routesFromCurrent = new Route[] {r11, r12, r21, r22, r31, r32};
-		currentIsland.setRouteArray(routesFromCurrent);
-		// Create a player
-		Player player1 = new Player("ben", 100, 30, currentIsland);
-		// Create a game environment
-		Island[] islands = new Island[] {currentIsland, i1, i2, i3};
-		GameEnvironment game1 = new GameEnvironment(player1, islands);
-		// test viewOtherIslands
-		game1.chooseOtherIsland();
-		
-		// note from me: fyi there needs to be 5 islands
-		// game1.takeTurn();
-	}	
+}	
 	// TODO need to have a method that handles a player not having any cash
-}
+
