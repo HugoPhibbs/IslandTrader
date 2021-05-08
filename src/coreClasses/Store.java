6252ccbb8febb9541ce0ed0f1eea1b06ec02917a
hugophibbs <81545721+hugophibbs@users.noreply.github.com>
2021-05-08 14:17:39 +1200

    Changes to Store
    added functionality around selling items to player. (no buying from
    player). although this could be implemented!.
    
    specifically:
    added a method canSellUpgrade() which opertates the same way as
    canSellItem(), but specific for upgrades.

--- src/coreClasses/Store.java
+++ src/coreClasses/Store.java
@@ -1,6 +1,6 @@
 package coreClasses;
 
-import java.lang.reflect.Array; 
+import java.lang.reflect.Array;  
 
 import java.util.*;
 
@@ -37,55 +37,90 @@ public class Store {
     	this.buyCatalogue = buyCatalogue;
     }
   
-    // #################### GENERAL STORE METHODS #####################
-    
+    // #################### METHODS FOR SELLING AN ITEM TO A PLAYER #####################
+
     /** Creates and sells and item to a player 
      * 
      * @param itemName String for the name of Item to be created
      * @param player PLayer object to receive Item
      * @return Boolean, if transaction was successful
      */
-    public Item sellItem(String itemName, Player player) {
-    	
-    	// Get price of item in sellCatalogue, and check that player has enough money
-    	
-    	
+    public Item sellItemToPlayer(String itemName, Player player) {
     	// returns the item that was sold either way, this is to make handling alternative flow
     	// ALOT easier!
     	if (sellCatalogue.get(itemName) == null) {
     		throw new IllegalStateException("BUG store does not sell this item!");
     	}
     	
-    	Item itemToSell = new Item(itemName, sellCatalogue.get(itemName).get("spaceTaken"), sellCatalogue.get(itemName).get("price") );
+    	// check whether itemName is an upgrade, then call a helper method to handle. 
     	// TODO need to discard object if it doesnt work? or will java do it automatically??
-    	if (!sellItemChecker(player, itemToSell).equals("can sell")){
+    	
+    	
+    	Item itemToSell = new Item(itemName, sellCatalogue.get(itemName).get("spaceTaken"), sellCatalogue.get(itemName).get("price"));
+    	
+    	if (!canSellItem(player, itemToSell).equals("Can sell")){
     		return itemToSell; // handled by ui
     	}
     	
+    	if (itemName.endsWith("(upgrade)")) {
+    		// cast down to a shipUpgrade, using inheritance.
+    		ShipUpgrade upgradeToSell = (ShipUpgrade) itemToSell; 
+    		// set defenseCapability of new upgradeToSellObject
+    		upgradeToSell.setDefenseBoost(sellCatalogue.get(upgradeToSell.getName()).get("defenseBoost"));
+    		if (!canSellUpgrade(player).equals("Can sell")) {
+    			return (Item) upgradeToSell;
+    		}
+    		player.getShip().addUpgrade(upgradeToSell);
+    	}
+    	else { // regular item
+    		player.getShip().addItem(itemToSell);
+    	}
+    	
+    	// set with player, used by ui to check if transaction was completed
     	itemToSell.setWithPlayer(true);
     	player.spendMoney(itemToSell.getPlayerBuyPrice());
-    	player.getShip().addItem(itemToSell);
     	player.addPurchasedItem(itemToSell);
-    	
-    	return itemToSell;
+    
+    	return itemToSell; // note that if we handled a shipUpgrade, it would return it. 
     }
     
     
-    /** Checks if selling an item from a STORE TO A PLAYER is permissible
-     *  if it is, returns null, otherwise returns String for reason why not
+    // only can sell ship upgrades, and these count as items, against the quota of total items that a ship can carry
+    // put a wrapper class to selling Items and Upgrades (super class vs child class)
+    
+    // how to access upgrades, should these be in their own catalogue, doesnt really make any sense to do it like that!
+    
+    /** Checks if selling an item from a store is permissible
      * 
-     * @param player
-     * @return
+     * @param player Player object that item is trying to be sold to 
+     * @param itemToSell Item being checked if it can be sold 
+     * @return String representation if item can be sold or not, along with a reason if not. 
      */
-    public static String sellItemChecker(Player player, Item itemToSell) {
+    public static String canSellItem(Player player, Item itemToSell) {
     	if (player.getMoneyBalance() < itemToSell.getPlayerBuyPrice()) {
-    		return "Player does not have enough money to buy this item!";
+    		return "Can't sell Item, Player does not have enough money to buy this item!";
     	}
-    	else if (player.getShip().getRemainingCargoCapacity() < itemToSell.getSpaceTaken()) {
-    		return "Player does not have enough space to store this item!";
+    	else if (player.getShip().getRemainingItemSpace() < itemToSell.getSpaceTaken()) {
+    		return "Can't sell Item, Player does not have enough space to store this item!";
     	}
-    	return "can sell";
+    	return "Can sell";
     }
+    
+    /** Checks if a store can sell an upgrade
+     * 
+     * @param player Player object that upgrade is trying to be sold to 
+     * @param upgradeToSell ShipUpgrade object that is trying to be sold
+     * @return Boolean if ShipUpgrade object can be sold. 
+     */
+    public static String canSellUpgrade(Player player) {
+    	// other checking is done by canSellItem Method above
+    	if (player.getShip().getDefenseCapability() < 50) {
+    		return "Can't sell Upgrade, Ship already has max defense Capability";
+    	}
+    	return "Can sell";
+    }
+    
+    // ############## METHODS FOR BUYING AN ITEM FROM A PLAYER #################
     
     /** Buys an item from a Player to a store
      * 
@@ -93,14 +128,14 @@ public class Store {
      * @param player Player object that is selling an Item
      * @return Boolean if transaction was successful
      */
-    public Item buyItem(String itemName, Player player) {
+    public Item buyItemFromPlayer(String itemName, Player player) {
     	if (buyCatalogue.get(itemName) == null) {
     		throw new IllegalStateException("BUG store does not buy this item!");
     	}
     	
     	Item itemToBuy = player.getShip().takeItem(itemName);
     	
-    	if (buyItemChecker(player, itemToBuy) != null) {
+    	if (!buyItemChecker(player, itemToBuy).equals("Can buy")) {
     		// not successful, return item, and dont remove it from players possession
     		return itemToBuy;
     	}
@@ -126,7 +161,7 @@ public class Store {
     	if (itemToBuy == null) {
     		return "Player does not have this item in possession!";
     	}
-    	return "can buy";
+    	return "Can buy";
     }
     
     /** Converts a sell or buy catalogue into an an Array List that can be easily displayed
@@ -170,6 +205,41 @@ public class Store {
     }
     // ##################### GETTER METHODS ########################
     
+    /** Gets the visit options for a store, used by ui
+     * 
+     * @return ArrayList for the options for visiting a store
+     */
+    public static String [] getVisitOptions() {
+    	String[] optionsArray = 
+    		    {"View and buy items that the store sells." 
+				,"View and sell Items that the store buys."
+				,"View previously bought items."
+				,"View the amount of money that you have."};
+    	return optionsArray;
+    }
+    
+    /** Gets the name of a chosen item to be sold or bought
+     * 
+     * @param displayArrayList ArrayList that details items that can be sold or bought
+     * @param chosenItemNum ItemNum action int chosen by cmd line ui of chosen item
+     * @return
+     */
+    public static String getChosenItemName(ArrayList<String> displayArrayList, int chosenItemNum) {
+    	// gets the name of a chosen, as in implemented code from game environment, just nice to have here
+    	// display ArrayList must have form {"itemName for ..."}, key thing being itemName is in first position
+    	
+    	return (String) Array.get(displayArrayList.get(chosenItemNum-1).split(" "), 0);
+    }
+    
+    /** Gets the description of the store
+     * 
+     * @return String for the description of the store
+     */
+    public String getDescription(){
+    	// specific things that a store sells will be given on request!
+    	return String.format("Store %s, is located on %s and specialises in %s!", name, storeIsland.getIslandName(), specialty);
+    }
+    
     /** Gets the name of the store
      * 
      * @return String for the name of the store
@@ -199,41 +269,6 @@ public class Store {
      * @return HashMap<String, HashMap<String, Integer>> representation of the things that a store buys
      */
     public HashMap<String, HashMap<String, Integer>> getBuyCatalogue() {return buyCatalogue;}
-    
-    /** Gets the description of the store
-     * 
-     * @return String for the description of the store
-     */
-    public String getDescription(){
-    	// specific things that a store sells will be given on request!
-    	return String.format("Store %s, is located on %s and specialised in %s!", name, storeIsland.getIslandName(), specialty);
-    }
-    
-    /** Gets the visit options for a store, used by ui
-     * 
-     * @return ArrayList for the options for visiting a store
-     */
-    public static String [] getVisitOptions() {
-    	String[] optionsArray = 
-    		    {"View and buy items that the store sells." 
-				,"View and sell Items that the store buys."
-				,"View previously bought items."
-				,"View the amount of money that you have."};
-    	return optionsArray;
-    }
-    
-    /** Gets the name of a chosen item to be sold or bought
-     * 
-     * @param displayArrayList ArrayList that details items that can be sold or bought
-     * @param chosenItemNum ItemNum action int chosen by cmd line ui of chosen item
-     * @return
-     */
-    public static String getChosenItemName(ArrayList<String> displayArrayList, int chosenItemNum) {
-    	// gets the name of a chosen, as in implemented code from game environment, just nice to have here
-    	// display ArrayList must have form {"itemName for ..."}, key thing being itemName is in first position
-    	
-    	return (String) Array.get(displayArrayList.get(chosenItemNum-1).split(" "), 0);
-    }
     
     // ##################### SETTER METHODS ########################
     
