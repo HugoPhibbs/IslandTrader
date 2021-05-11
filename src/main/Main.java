ff5d5e6f13c087c1aafd170f68cc49dcdee1e645
hugophibbs <81545721+hugophibbs@users.noreply.github.com>
2021-05-11 18:34:35 +1200

    Changes to main
    reorganized code.
    now methods for createIslands and createRoutes
    
    makes it easier to read
    
    add skeleton code for further creatingnbuy and sell catalogues for every
    island, this is going to take alot of copying and pasting

--- src/main/Main.java
+++ src/main/Main.java
@@ -26,13 +26,45 @@ public class Main {
 	
 	public static void main(String[] args) {
 		// Create objects required to initiate GameEnvironement
+		
 		// Create Ships to choose from, and an array to store
 		Ship[] shipArray = createShips();
 		
-		// Create the first island and its store
+		// Create buy and sell catalogues
 		ArrayList<HashMap<String, HashMap<String, Integer>>> buyCatalogues = createBuyCatalogues();
 		ArrayList<HashMap<String, HashMap<String, Integer>>> sellCatalogues = createSellCatalogues();
 		
+		// Create an island array, required for game environment constructor
+		Island[] islands = createIslands(buyCatalogues, sellCatalogues);
+	
+		// Create Routes
+		Route[] routes = createRoutes(islands);
+		
+		for (Island island: islands) {
+			List<Route> routesToIsland = new ArrayList<Route>();
+			for(Route route: routes) {
+				if (island == route.getIslands()[0] || island == route.getIslands()[1]) {
+					routesToIsland.add(route);
+				}
+			}
+			// Convert route list to route array
+			Route[] routeArray = new Route[routesToIsland.size()];
+			for (int i = 0; i < routesToIsland.size(); i++) {
+				routeArray[i] = routesToIsland.get(i);
+			}
+			island.setRouteArray(routeArray);
+		}
+		
+		// Initiate the UI and Game Environment
+		GameUi ui;
+		// TODO: needs to be changed later to allow both UIs to work.
+		// THis is however easier for testing the command line user interface
+		ui = new CmdLineUi();
+		GameEnvironment gameEnrviron = new GameEnvironment(islands, shipArray, ui);
+		ui.setup(gameEnrviron);
+	}
+	
+	public static Island[] createIslands(ArrayList<HashMap<String, HashMap<String, Integer>>> buyCatalogues, ArrayList<HashMap<String, HashMap<String, Integer>>> sellCatalogues) {
 		Store cyprusStore = new Store("War Goods", "Pies", buyCatalogues.get(0), sellCatalogues.get(0)); 
 		Island cyprus = new Island("Cyprus", cyprusStore, "arb description");
 		cyprusStore.setStoreIsland(cyprus);
@@ -57,47 +89,52 @@ public class Main {
 		Island ibiza = new Island("Ibiza", ibizaStore, "arb3"); 
 		ibizaStore.setStoreIsland(ibiza);
 		
-	
-		// Create Routes
-		Route cyprusAndSicily = new Route("firstRoute", 10, new Island[] {cyprus, sicily}, "test 1"); cyprusAndSicily.constructProbabilityMap(50, 50, 50);
-		Route cyprusAndCorsica = new Route("secondRoute", 20, new Island[] {cyprus, corsica}, "test 2"); cyprusAndCorsica.constructProbabilityMap(50, 50, 50);
-		Route cyprusAndMalta = new Route("firstRoute", 10, new Island[] {cyprus, malta}, "test 1"); cyprusAndMalta.constructProbabilityMap(50, 50, 50);
-		Route cyprusAndIbiza = new Route("secondRoute", 20, new Island[] {cyprus, ibiza}, "test 2"); cyprusAndIbiza.constructProbabilityMap(50, 50, 50);
-		Route sicilyAndIbiza = new Route("firstRoute", 10, new Island[] {sicily, ibiza}, "test 1"); sicilyAndIbiza.constructProbabilityMap(50, 50, 50);
-		Route sicilyAndCorsica = new Route("secondRoute", 20, new Island[] {sicily, corsica}, "test 2"); sicilyAndCorsica.constructProbabilityMap(50, 50, 50);
-		Route sicilyAndMalta = new Route("firstRoute", 10, new Island[] {sicily, malta}, "test 1"); sicilyAndMalta.constructProbabilityMap(50, 50, 50);
-		Route maltaAndCorsica = new Route("firstRoute", 10, new Island[] {malta, corsica}, "test 1"); maltaAndCorsica.constructProbabilityMap(50, 50, 50);
-		Route maltaAndIbiza = new Route("firstRoute", 10, new Island[] {malta, ibiza}, "test 1"); maltaAndIbiza.constructProbabilityMap(50, 50, 50);
-		Route ibizaAndCorsica = new Route("firstRoute", 10, new Island[] {ibiza, corsica}, "test 1"); ibizaAndCorsica.constructProbabilityMap(50, 50, 50);
-		
-		// Create an island array, required for game environment constructor
 		Island[] islands = new Island[] {cyprus, sicily, corsica, malta, ibiza};
 		
-		// Assign islands to routes
-		Route[] allRoutes = new Route[] {cyprusAndSicily, cyprusAndCorsica, cyprusAndMalta, cyprusAndIbiza, sicilyAndIbiza, sicilyAndCorsica, 
-				sicilyAndMalta, maltaAndCorsica, maltaAndIbiza, ibizaAndCorsica};
-		for (Island island: islands) {
-			List<Route> routesToIsland = new ArrayList<Route>();
-			for(Route route: allRoutes) {
-				if (island == route.getIslands()[0] || island == route.getIslands()[1]) {
-					routesToIsland.add(route);
-				}
-			}
-			// Convert route list to route array
-			Route[] routeArray = new Route[routesToIsland.size()];
-			for (int i = 0; i < routesToIsland.size(); i++) {
-				routeArray[i] = routesToIsland.get(i);
-			}
-			island.setRouteArray(routeArray);
-		}
+		return islands;
+	}
+	
+	public static Route[] createRoutes(Island[] islands) {
+		Island cyprus = islands[0];
+		Island sicily = islands[1];
+		Island corsica = islands[2];
+		Island malta = islands[3];
+		Island ibiza = islands[4];
 		
-		// Initiate the UI and Game Environment
-		GameUi ui;
-		// TODO: needs to be changed later to allow both UIs to work.
-		// THis is however easier for testing the command line user interface
-		ui = new CmdLineUi();
-		GameEnvironment gameEnrviron = new GameEnvironment(islands, shipArray, ui);
-		ui.setup(gameEnrviron);
+		Route cyprusAndSicily = new Route("firstRoute", 10, new Island[] {cyprus, sicily}, "test 1"); 
+		cyprusAndSicily.constructProbabilityMap(50, 50, 50);
+		
+		Route cyprusAndCorsica = new Route("secondRoute", 20, new Island[] {cyprus, corsica}, "test 2"); 
+		cyprusAndCorsica.constructProbabilityMap(50, 50, 50);
+		
+		Route cyprusAndMalta = new Route("firstRoute", 10, new Island[] {cyprus, malta}, "test 1"); 
+		cyprusAndMalta.constructProbabilityMap(50, 50, 50);
+		
+		Route cyprusAndIbiza = new Route("secondRoute", 20, new Island[] {cyprus, ibiza}, "test 2"); 
+		cyprusAndIbiza.constructProbabilityMap(50, 50, 50);
+		
+		Route sicilyAndIbiza = new Route("firstRoute", 10, new Island[] {sicily, ibiza}, "test 1"); 
+		sicilyAndIbiza.constructProbabilityMap(50, 50, 50);
+		
+		Route sicilyAndCorsica = new Route("secondRoute", 20, new Island[] {sicily, corsica}, "test 2"); 
+		sicilyAndCorsica.constructProbabilityMap(50, 50, 50);
+		
+		Route sicilyAndMalta = new Route("firstRoute", 10, new Island[] {sicily, malta}, "test 1"); 
+		sicilyAndMalta.constructProbabilityMap(50, 50, 50);
+		
+		Route maltaAndCorsica = new Route("firstRoute", 10, new Island[] {malta, corsica}, "test 1"); 
+		maltaAndCorsica.constructProbabilityMap(50, 50, 50);
+		
+		Route maltaAndIbiza = new Route("firstRoute", 10, new Island[] {malta, ibiza}, "test 1");
+		maltaAndIbiza.constructProbabilityMap(50, 50, 50);
+		
+		Route ibizaAndCorsica = new Route("firstRoute", 10, new Island[] {ibiza, corsica}, "test 1"); 
+		ibizaAndCorsica.constructProbabilityMap(50, 50, 50);
+		
+		Route[] routes = new Route[] {cyprusAndSicily, cyprusAndCorsica, cyprusAndMalta, cyprusAndIbiza, sicilyAndIbiza, sicilyAndCorsica, 
+				sicilyAndMalta, maltaAndCorsica, maltaAndIbiza, ibizaAndCorsica};
+		
+		return routes;
 	}
 	
 	public static ArrayList<HashMap<String, HashMap<String, Integer>>> createBuyCatalogues(){
@@ -122,10 +159,19 @@ public class Main {
 		buyCatalogue.put("Canon(upgrade)", canonProperties);
 		ArrayList<HashMap<String, HashMap<String, Integer>>> catalogues = new ArrayList<HashMap<String, HashMap<String, Integer>>>();
 		catalogues.add(buyCatalogue);
+		
+		// Create buy catalogue for sicily
+		
+		// Create buy catalogue for corsica
+		
+		// Create buy catalogue for malta
+		
+		// Create buy catalogue for ibiza
 		return catalogues;
 	}
 	
 	public static ArrayList<HashMap<String, HashMap<String, Integer>>> createSellCatalogues() {
+		// Create sell catalogue for cyprus
 		HashMap<String, HashMap<String, Integer>> sellCatalogue = new HashMap<String, HashMap<String, Integer>>();
 		HashMap<String, Integer> goldProperties = new HashMap<String, Integer>();
 		goldProperties.put("spaceTaken", 2);
@@ -141,14 +187,23 @@ public class Main {
 		sellCatalogue.put("Banana", bananaProperties);
 		ArrayList<HashMap<String, HashMap<String, Integer>>> catalogues = new ArrayList<HashMap<String, HashMap<String, Integer>>>();
 		catalogues.add(sellCatalogue);
+		
+		// Create sell catalogue for sicily
+		
+		// Create sell catalogue for corsica
+		
+		// Create sell catalogue for malta
+		
+		// Create sell catalogue for ibiza
 		return catalogues;
 	}
 	
 	public static Ship[] createShips() {
-		Ship ship1 = new Ship("Black Pearl", 100, 20, 50);
-		Ship ship2 = new Ship("Thunder Bird", 80, 30, 40);
-		Ship ship3 = new Ship("Batmobile", 70, 10, 70);
-		Ship ship4 = new Ship("Apollo", 100, 30, 40);
+		// TODO messed up the numbers bellow, pls check again
+		Ship ship1 = new Ship("Black Pearl", 100, 10, 30);
+		Ship ship2 = new Ship("Thunder Bird", 80, 8, 40);
+		Ship ship3 = new Ship("Batmobile", 70, 7, 50);
+		Ship ship4 = new Ship("Apollo", 100, 6, 20);
 		return new Ship[] {ship1, ship2, ship3, ship4};
 	}
 }
