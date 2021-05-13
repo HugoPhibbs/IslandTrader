d8c6cdc4a8c815445cc82967589c062a71124117
Jordan <jve31@uclive.ac.nz>
2021-05-14 11:35:55 +1200

    Bin files

--- src/uiClasses/gui/Screen.java
+++ src/uiClasses/gui/Screen.java
@@ -2,9 +2,21 @@ package uiClasses.gui;
 
 import javax.swing.*;
 
+import coreClasses.GameEnvironment;
+
 
 public abstract class Screen {
 	
 	private final JFrame frame;
+	
+	private final GameEnvironment game;
+	
+	protected Screen(String title, GameEnvironment gameEvironment) {
+		this.game = gameEvironment;
+		iniitalise(title);
+	}
+	
+	
+	}
 
 }
