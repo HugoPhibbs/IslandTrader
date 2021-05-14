ad4753e6e92f9bd96b41090ab24ad12f514b4aaf
Jordan <jve31@uclive.ac.nz>
2021-05-15 09:36:11 +1200

    Updated playrTests
    Currently minimal testing, need to be clear with what you have changed
    before I add more.

--- src/junitTests/PlayerTests.java
+++ src/junitTests/PlayerTests.java
@@ -5,6 +5,8 @@ package junitTests;
 
 import coreClasses.*;
 import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 /**
@@ -13,12 +15,20 @@ import org.junit.jupiter.api.Test;
  */
 class PlayerTests { 
 	
+	private Player testPlayer;
+	private Ship testShip;
 	
-	// constructor test requires ship
-	@Test
-	void constructorTest() {
-		
-		
+	
+	@BeforeEach
+	void createRequiredObjects() {
+		testShip = new Ship("Black Pearl", 10,  5, 30);
+		testPlayer = new Player("Jack Sparrow", 1000);
+		testPlayer.setShip(testShip);
 	}
-
+	
+	@Test
+	void testMoneyBalanceToString() {
+		assertEquals("You have a balance of: 1000 pirate bucks", testPlayer.moneyBalanceToString());
+	}
+	
 }
