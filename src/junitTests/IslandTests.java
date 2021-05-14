1a2aa33f2091f959c30462db50c8baa3bf614490
Jordan <jve31@uclive.ac.nz>
2021-05-15 10:40:18 +1200

    JUnit tests for Island
    Full JUnit tests for island, testing all methods that contain any logic.
    
    Does not test arbitrary setters or getters.

--- src/junitTests/IslandTests.java
+++ src/junitTests/IslandTests.java
@@ -5,8 +5,14 @@ package junitTests;
 
 import coreClasses.*; 
 import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import java.io.ByteArrayInputStream;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
 
 /**
  * @author Jordan Vegar
@@ -14,34 +20,100 @@ import java.io.ByteArrayInputStream;
  */
 class IslandTests {
 	
+	private Island mainIsland;
+	private Island testIsland1;
+	private Island testIsland2;
+	private Route mainAndTest1Fun;
+	private Route mainAndTest1Scary;
+	private Route mainAndTest2;
+	
+	
+
 	
 	// Note will probably transfer the code that sets up the stores islands and routes to a before each/all method. 
-	@Test
-	void viewRoutesTest() {
-		// creating instances of island to create routes to for testing
-		Store s1 = new Store(); Store s2 = new Store(); Store s3 = new Store(); 
-		Island i1 = new Island("other 1", s1, "arb1"); 
-		Island i2 = new Island("other 2", s2, "arb2"); 
-		Island i3 = new Island("other 3", s3, "arb3"); 
-		// Creating current instance of Island
-		Store currStore = new Store(); 
-		Island currentIsland = new Island("current", currStore, "arb description");
-		// creating routes from the current island
-		Route r11 = new Route("r11", 3, currentIsland, i1, "wavy route, but with storms come sailors to save");
-		Route r12 = new Route("r12", 2, currentIsland, i1, "easy sailing, which pirates also enjoy");
-		Route r21 = new Route("r21", 2, currentIsland, i2, "safe route, low risk, low reward.");
-		Route r22 = new Route("r22", 2, currentIsland, i2, "wavy route, but with storms come sailors to save");
-		Route r31 = new Route("r31", 4, currentIsland, i3, "long route, takes a detour to find calmer waters");
-		Route r32 = new Route("r32", 1, currentIsland, i3, "Direct route with high winds, fast but lots of bad weather. ");
-		// create route array and pass to current island
-		Route[] routesFromCurrent = new Route[] {r11, r12, r21, r22, r31, r32};
-		currentIsland.setRouteArray(routesFromCurrent);
+	@BeforeAll
+	void createRequiredObjects() {
+		/*
+		 * Create the buy and sell catalogues for the main island's store, required so Island.getFullInfo() can be
+		 * throughly tested.
+		 */
+		// Sell catalogue
+		HashMap<String, HashMap<String, Integer>> testSellCatalogue = new HashMap<String, HashMap<String, Integer>>();
+		HashMap<String, Integer> tomatoProperties = new HashMap<String, Integer>();
+		tomatoProperties.put("spaceTaken", 1);
+		tomatoProperties.put("price", 3);
+		HashMap<String, Integer> rumProperties = new HashMap<String, Integer>();
+		rumProperties.put("spaceTaken", 3);
+		rumProperties.put("price", 4);
+		testSellCatalogue.put("Tomato", tomatoProperties);
+		testSellCatalogue.put("Rum", rumProperties);
+		// buy cataogue
+		HashMap<String, HashMap<String, Integer>> testBuyCatalogue = new HashMap<String, HashMap<String, Integer>>();
+		HashMap<String, Integer> goldProperties = new HashMap<String, Integer>();
+		goldProperties.put("spaceTaken", 0);
+		goldProperties.put("price", 30);
+		HashMap<String, Integer> limeProperties = new HashMap<String, Integer>();
+		limeProperties.put("spaceTaken", 0);
+		limeProperties.put("price", 10);
+		testBuyCatalogue.put("Gold", goldProperties);
+		testBuyCatalogue.put("Lime", limeProperties);
 		
-		// How to test properly for void method that only prints, doesn't return?
-		ByteArrayInputStream in1 = new ByteArrayInputStream("r11".getBytes());
-		System.setIn(in1);
-		assertEquals(r11, currentIsland.chooseRoute(i1));
 		
+		Store mainStore = new Store("Main Store", "This store is sells and buys things!", testSellCatalogue, testBuyCatalogue);
+		mainIsland = new Island("Main Island", mainStore, "An island with a nice store and even nicer tests");
+		
+		// Create other islands so the main isalnd t be tested has routes.
+		Store testStore1 = new Store("TestIsland1", "sorry, we are closed", null, null);
+		testIsland1 = new Island("Test1", testStore1, "Island exists only for its routes :(");
+		
+		Store testStore2 = new Store("TestIsland1", "sorry, we are closed", null, null);
+		testIsland2 = new Island("Test1", testStore2, "Island exists only for its routes :(");
+		
+		// Create routes between the main island and the test islands.
+		Island[] islands1 = new Island[] {mainIsland, testIsland1};
+		mainAndTest1Fun = new Route("fun route", 100, islands1, "fun!");
+		mainAndTest1Scary = new Route("scary route", 150, islands1, "scary!");
+		mainAndTest2 = new Route("only route", 200, new Island[] {testIsland2, mainIsland}, "ur only choice");
+		mainIsland.setRouteArray(new Route[] {mainAndTest1Fun, mainAndTest1Scary, mainAndTest2});
 	}
-
+	
+	@Test
+	void testToString() {
+		assertEquals("Main Island: An island with a nice store and even nicer tests", mainIsland.toString());
+	}
+	
+	@Test
+	void testGetPossibleRoutes() {
+		ArrayList<Route> expectedResult1 = new ArrayList<Route>(List.of(mainAndTest1Fun, mainAndTest1Scary));
+		assertEquals(expectedResult1, mainIsland.getPossibleRoutes(testIsland1));
+		
+		ArrayList<Route> expectedResult2 = new ArrayList<Route>(List.of(mainAndTest2));
+		assertEquals(expectedResult1, mainIsland.getPossibleRoutes(testIsland2));
+	}
+	
+	@Test
+	void testViewRoutes() {
+		ArrayList<Route> routesToView1 = new ArrayList<Route>(List.of(mainAndTest1Fun, mainAndTest1Scary));
+		String expectedResult1 = "Routes to Test1:\n - fun route: Is 100 km long. fun!\n - scary route: Is 150 km long. scary!";
+		assertEquals(expectedResult1, mainIsland.getPossibleRoutes(testIsland1));
+	}
+	
+	@Test
+	void testGetFullInfo() {
+		ArrayList<Route> routesFromTest1toMain = new ArrayList<Route>(List.of(mainAndTest1Fun, mainAndTest1Scary));
+		String expectedMainInfoString = "About Main Island: An island with a nice store and even nicer tests\n"
+				+ "Routes to Main Island:\n - fun route: Is 100 km long. fun!\n - scary route: Is 150 km long. scary!"
+				+ "The store on this island sells:\nTomato; Rum; \n"
+				+ "The store on this island buys:\nGold; Lime; \n";
+		
+		assertEquals(expectedMainInfoString, mainIsland.getFullInfo(routesFromTest1toMain));
+	}
 }
+
+
+
+
+
+
+
+
