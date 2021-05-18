d568173a3af711ae052ca227555850dacc8d99b5
hph41 <hph41@cs19106bs.canterbury.ac.nz>
2021-05-18 20:24:35 +1200

    Updates to Screen
    Added basic skeleton as per the UML diagram that was made by U

--- src/uiClasses/gui/Screen.java
+++ src/uiClasses/gui/Screen.java
@@ -1,5 +1,7 @@
 package uiClasses.gui;
 
+import java.awt.Container;
+
 import javax.swing.*;
 
 import coreClasses.GameEnvironment;
@@ -7,16 +9,43 @@ import coreClasses.GameEnvironment;
 
 public abstract class Screen {
 	
-	private final JFrame frame;
-	
 	private final GameEnvironment game;
+	private final Screen parent;
 	
-	protected Screen(String title, GameEnvironment gameEvironment) {
+	protected Screen(String title, GameEnvironment gameEvironment, Screen parent) {
 		this.game = gameEvironment;
-		iniitalise(title);
+		this.parent = parent;
+		initialize(title);
 	}
 	
+	abstract protected void initialize(String title);
 	
+	
+	protected GameEnvironment getGame() {
+		return game;
 	}
-
+	
+	protected void show() {
+		
+	}
+	
+	protected boolean confirmQuit() {
+		return true;
+	}
+	
+	public void quit() {
+		
+	}
+	
+	public void goBack() {
+		
+	}
+	
+	public void showError() {
+		
+	}
+	
+	public Screen getParent() {
+		return parent;
+	}
 }
