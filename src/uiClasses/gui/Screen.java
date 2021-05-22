f92f3ea41af128cc6214ede9d716bc3d421368ea
hugophibbs <81545721+hugophibbs@users.noreply.github.com>
2021-05-22 17:21:18 +1200

    Changes to Screen
    added a method hide() which does the opposite of show()

--- src/uiClasses/gui/Screen.java
+++ src/uiClasses/gui/Screen.java
@@ -78,6 +78,10 @@ public abstract class Screen {
 		frame.setVisible(true);
 	}
 	
+	protected void hide() {
+		frame.setVisible(false);
+	}
+	
 	/**
 	 * Displays a Dialog to allow the user to confirm whether they would like to quit
 	 * @return Boolean true if would like to quit, else false
