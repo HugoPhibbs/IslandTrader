c5e03121ce40e084b0eb361284e6dcffd2e65f7a
Jordan <jve31@uclive.ac.nz>
2021-05-27 14:42:34 +1200

    Javadoc

--- doc/package-search-index.js
+++ doc/package-search-index.js
@@ -1,1 +1,1 @@
-packageSearchIndex = [{"l":"All Packages","u":"allpackages-index.html"},{"l":"coreClasses"},{"l":"junitTests"},{"l":"main"},{"l":"uiClasses"},{"l":"uiClasses.gui"}];updateSearchResults();
+packageSearchIndex = [{"l":"All Packages","url":"allpackages-index.html"},{"l":"coreClasses"},{"l":"main"},{"l":"uiClasses"},{"l":"uiClasses.gui"}]
